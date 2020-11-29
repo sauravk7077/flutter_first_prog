c7405e3ef5a86833a2ee00bfffd6175506742d4a
Saurav Kumar <sauravk7077@gmail.com>
2020-11-29 10:56:24 +0530

    row column and containers

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Hey there"),
-        centerTitle: true,
+          title: Text(
+            "Hey there",
+            style: TextStyle(fontFamily: "EvilEmpire"),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[600]),
+      body: Column(
+        children: [
+          Text("Text"),
+          ElevatedButton(onPressed: () {}, child: Text("Button")),
+          Container(
+            child: Text("Inside a container"),
+            color: Colors.blue,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Hello"),
-        ),
-      ),
+      // Row(
+      //   children: [
+      //     Text("Hello"),
+      //     ElevatedButton(onPressed: () {}, child: Text("Button")),
+      //     Container(
+      //       child: Text("Inside a container"),
+      //       color: Colors.blue,
+      //       padding: EdgeInsets.all(10),
+      //       margin: EdgeInsets.all(10),
+      //     )
+      //   ],
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      // ),
       floatingActionButton: FloatingActionButton(
         child: Text("Click"),
+        onPressed: () {
+          print("hello");
+        },
+        backgroundColor: Colors.red[600],
       ),
-    ),
-  ));
+    );
+  }
 }
